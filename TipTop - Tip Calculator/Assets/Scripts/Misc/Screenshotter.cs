1a7edaea9e68235fdbeee09a6b530e88f7f4416b
DiamonickKCS <46506748+diamonick@users.noreply.github.com>
2024-01-29 09:50:04 -0600

    Implement taking screenshots in Unity editor

---
+++ TipTop - Tip Calculator/Assets/Scripts/Misc/Screenshotter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshotter : MonoBehaviour
+{
+    public int count = 0;
+    public bool enableScreenshot;
+
+    private void Update()
+    {
+        if (Input.touchCount > 0 && enableScreenshot)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                ScreenCapture.CaptureScreenshot(Application.dataPath + $"/Screenshots/TipTop_IPhone_Photo{count++}.png");
+            }
+        }
+    }
+}
