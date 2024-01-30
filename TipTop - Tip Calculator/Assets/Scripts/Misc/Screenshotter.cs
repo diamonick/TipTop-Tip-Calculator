71b8132cae2367aa9debba25be4428027ce776ae
DiamonickKCS <46506748+diamonick@users.noreply.github.com>
2024-01-30 13:44:13 -0600

    Test screenshot capture feature

--- TipTop - Tip Calculator/Assets/Scripts/Misc/Screenshotter.cs
+++ TipTop - Tip Calculator/Assets/Scripts/Misc/Screenshotter.cs
@@ -4,19 +4,56 @@ using UnityEngine;
 
 public class Screenshotter : MonoBehaviour
 {
-    public int count = 0;
+    public enum Display
+    {
+        iPhone6_5 = 0,      // iPhone - 6.5" Display
+        iPhone5_5 = 1,      // iPhone - 5.5" Display
+        iPadPro_6Gen = 2,   // iPad Pro (6th Gen) - 12.9" Display
+        iPadPro_2Gen = 3,   // iPad Pro (2nd Gen) - 12.9" Display
+    }
+
     public bool enableScreenshot;
+    public Display display;
+    public int count = 0;
 
     private void Update()
     {
         if (Input.touchCount > 0 && enableScreenshot)
         {
+            string displayName = GetDisplayName();
             Touch touch = Input.GetTouch(0);
 
             if (touch.phase == TouchPhase.Began)
             {
-                ScreenCapture.CaptureScreenshot(Application.dataPath + $"/Screenshots/TipTop_Iphone_5.5_Photo{count++}.png");
+                ScreenCapture.CaptureScreenshot(Application.dataPath + $"/Screenshots/TipTop_{displayName}_Photo{count++}.png");
             }
         }
+    }
+
+    private string GetDisplayName()
+    {
+        string displayName = "";
+
+        switch (display)
+        {
+            // iPhone - 6.5" Display
+            case Display.iPhone6_5:
+                displayName = "iPhone_6.5";
+                break;
+            // iPhone - 5.5" Display
+            case Display.iPhone5_5:
+                displayName = "iPhone_5.5";
+                break;
+            // iPad Pro (6th Gen) - 12.9" Display
+            case Display.iPadPro_6Gen:
+                displayName = "iPadPro_(6th Gen)";
+                break;
+            // iPad Pro (2nd Gen) - 12.9" Display
+            case Display.iPadPro_2Gen:
+                displayName = "iPadPro_(2nd Gen)";
+                break;
+        }
+
+        return displayName;
     }
 }
